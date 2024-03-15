d65f0ad7ac2cec021f54ce127e6544372045cfc8
SHUAXIN <shuaxinjs@qq.com>
2024-03-15 14:16:06 +0800

    feat: 定位组件插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codeInspectorPlugin({
+      bundler: "vite",
+    }),
+  ],
 });
